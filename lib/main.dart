52ac175cdc54c707045b24d7608da01755cc9505
aswinpradeepc <aswinpradeepc@gmail.com>
2024-11-24 13:28:05 +0530

    add icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:counterplus/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
   runApp(MyApp());
 }
 
